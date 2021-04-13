7fb956c4923a48e176a2c24686883f37d9027a4a
deepti-06 <52527169+deepti-06@users.noreply.github.com>
2021-04-13 12:19:16 +0530

    Update hex.js

--- hex.js
+++ hex.js
@@ -1,14 +1,12 @@
 class Hex extends BaseClass {
     constructor(x,y){
       super(x,y,50,50);
-      this.image = loadImage("polygon.png");
+      //this.image = loadImage("polygon.png");
     }
   
     display() {
       //this.body.position.x = mouseX;
       //this.body.position.y = mouseY;
       super.display();
-        imageMode(CENTER)
-        image(this.image,0,0,50,50)
     }
   }
